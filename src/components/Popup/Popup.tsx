cd87f4f121cb71ef479876d76c8788eef0304d24
Francis Borja <49008508+fborja44@users.noreply.github.com>
2025-01-17 19:37:58 -0500

    feat: popup transition

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,4 +1,9 @@
-import { Dialog, DialogPanel, DialogTitle } from '@headlessui/react';
+import {
+	Dialog,
+	DialogBackdrop,
+	DialogPanel,
+	DialogTitle,
+} from '@headlessui/react';
 import { useEffect } from 'react';
 import useTagStore from '../../store/tagStore';
 import useAudio from '../../hooks/useAudio';
@@ -37,29 +42,31 @@ const Popup = ({
 				playAudio();
 			}}
 		>
-			<div className='fixed inset-0 z-10 w-screen overflow-y-auto bg-black/30 backdrop-blur-md duration-300'>
-				<div className='flex min-h-full items-center justify-center p-4'>
-					<DialogPanel
-						// transition
-						className='flex flex-col gap-4 max-w-2xl rounded-3xl bg-midnight-950 border-4 border-b-[10px] border-midnight-600 px-7 py-5 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0'
-					>
-						{(title || icon) && (
-							<DialogTitle
-								as='h2'
-								className='container-row gap-2 text-3xl text-menu-yellow'
-							>
-								{icon && <span className='relative top-0.5'>{icon}</span>}
-								{title && <span>{title}</span>}
-							</DialogTitle>
-						)}
-						<div className='w-full bg-black/50 rounded-2xl'>{children}</div>
-						{actions && (
-							<div className='container-row justify-center mt-1 w-full'>
-								{actions}
-							</div>
-						)}
-					</DialogPanel>
-				</div>
+			<div className='fixed inset-0 flex w-screen items-center justify-center p-4'>
+				<DialogBackdrop
+					transition
+					className='fixed inset-0 bg-black/50 backdrop-blur-md duration-[50ms] ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0'
+				/>
+				<DialogPanel
+					transition
+					className='flex flex-col gap-4 max-w-2xl rounded-3xl bg-midnight-950 border-4 border-b-[10px] border-midnight-600 px-7 py-5 backdrop-blur-2xl duration-[50ms] ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0'
+				>
+					{(title || icon) && (
+						<DialogTitle
+							as='h2'
+							className='container-row gap-2 text-3xl text-menu-yellow'
+						>
+							{icon && <span className='relative top-0.5'>{icon}</span>}
+							{title && <span>{title}</span>}
+						</DialogTitle>
+					)}
+					<div className='w-full bg-black/50 rounded-2xl'>{children}</div>
+					{actions && (
+						<div className='container-row justify-center mt-1 w-full'>
+							{actions}
+						</div>
+					)}
+				</DialogPanel>
 			</div>
 		</Dialog>
 	);
